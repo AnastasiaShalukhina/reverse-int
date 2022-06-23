26db25eab9ae38452c0fc3621685cce0f39eb9fc
AnastasiaShalukhina <exachka4@gmail.com>
2022-06-23 19:31:37 +0300

    add function to rvert int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = String(n);
+
+    if (str.substring(0, 1) === "-") {
+        return +str.slice(1).split("").reverse().join("");
+    } else {
+        return +str.split("").reverse().join("");
+    }
+};
